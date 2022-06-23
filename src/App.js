eca16bfd7acf4819d2470beb16db76872a0d6932
Myranae <myranae@pm.me>
2022-06-23 10:16:15 -0400

    gets to default

--- src/App.js
+++ src/App.js
@@ -4,67 +4,52 @@ import ClassInfo from "./components/ClassInfo";
 import NewStudentForm from "./components/NewStudentForm";
 
 function App() {
-  const [studentData, setStudentData] = useState([
-    {
-      id: 1,
-      nameData: "Ada",
-      emailData: "ada@dev.org",
-      isPresentData: false,
-    },
-    {
-      id: 2,
-      nameData: "Soo-ah",
-      emailData: "sooah@dev.org",
-      isPresentData: false,
-    },
-    {
-      id: 3,
-      nameData: "Chrissy",
-      emailData: "chrissy@dev.org",
-      isPresentData: true,
-    },
-  ]);
+    const [studentData, setStudentData] = useState([
+        {
+            id: 1,
+            nameData: "Ada",
+            emailData: "ada@dev.org",
+            isPresentData: false,
+        },
+        {
+            id: 2,
+            nameData: "Soo-ah",
+            emailData: "sooah@dev.org",
+            isPresentData: false,
+        },
+        {
+            id: 3,
+            nameData: "Chrissy",
+            emailData: "chrissy@dev.org",
+            isPresentData: true,
+        },
+    ]);
 
-  const addStudentData = (newStudent) => {
-    // Duplicate the student list
-    const newStudentList = [...studentData];
+    const updateStudentData = (updatedStudent) => {
+        const students = studentData.map((student) => {
+            if (student.id === updatedStudent.id) {
+                return updatedStudent;
+            } else {
+                return student;
+            }
+        });
 
-    // Logic to generate the next valid student ID
-    const nextId = Math.max(...newStudentList.map((student) => student.id)) + 1;
+        setStudentData(students);
+    };
 
-    newStudentList.push({
-      id: nextId,
-      nameData: newStudent.nameData,
-      emailData: newStudent.emailData,
-      isPresentData: false,
-    });
-
-    setStudentData(newStudentList);
-  };
-
-  const updateStudentData = (updatedStudent) => {
-    const students = studentData.map((student) => {
-      if (student.id === updatedStudent.id) {
-        return updatedStudent;
-      } else {
-        return student;
-      }
-    });
-
-    setStudentData(students);
-  };
-
-  return (
-    <main>
-      <h1>Attendance</h1>
-      <ClassInfo memberCount={studentData.length}></ClassInfo>
-      <StudentList
-        students={studentData}
-        onUpdateStudent={updateStudentData}
-      ></StudentList>
-      <NewStudentForm addStudentCallback={addStudentData}></NewStudentForm>
-    </main>
-  );
+    return (
+        <main>
+            <h1>Attendance</h1>
+            <ClassInfo memberCount={studentData.length}></ClassInfo>
+            <button onClick={() => setStudentData([])}>
+                Delete All Students
+            </button>
+            <StudentList
+                students={studentData}
+                onUpdateStudent={updateStudentData}
+            ></StudentList>
+        </main>
+    );
 }
 
 export default App;
--- src/components/ClassInfo.js
+++ src/components/ClassInfo.js
@@ -1,15 +1,20 @@
 import "./ClassInfo.css";
+import PropTypes from "prop-types";
 
 const ClassInfo = (props) => {
-  return (
-    <section>
-      <h2>Class Information</h2>
-      <ul>
-        <li>Name: Team Semicolons</li>
-        <li>Number of members: {props.memberCount}</li>
-      </ul>
-    </section>
-  );
+    return (
+        <section>
+            <h2>Class Information</h2>
+            <ul>
+                <li>Name: Team Semicolons</li>
+                <li>Number of members: {props.memberCount}</li>
+            </ul>
+        </section>
+    );
+};
+
+ClassInfo.propTypes = {
+    memberCount: PropTypes.number,
 };
 
 export default ClassInfo;
